4dd7d4ef9895f9f961ce6c457a96ba767ada66a6
Kyle Lin <kylejlin.us@gmail.com>
2018-10-29 20:29:19 -0700

    Make the log more descriptive

--- pkg/test.js
+++ pkg/test.js
@@ -1,3 +1,4 @@
 const mod = require('./console_error_panic_hook_test');
-mod.greet();
+// mod.greet();
+console.log('This is what an error looks like on node without the panic hook:\n');
 mod.panic();
